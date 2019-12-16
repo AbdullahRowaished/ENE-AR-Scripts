f7e8dda028da9bc3aab7498ff941398a68cf8ca4
Abdullah Rowaished <abdullah.rowaished.1@gmail.com>
2019-12-16 19:04:31 +0300

    refactor: Make internal GIS_Feature classes into public For accessibility, all GIS_Feature internal classes were made public.

--- GIS_Feature.cs
+++ GIS_Feature.cs
@@ -12,14 +12,14 @@ public class GIS_Feature
     /// <summary>
     /// Longitude and latitude for a Point
     /// </summary>
-    internal class Coordinates
+    public class Coordinates
     {
         public float longitude, latitude;
     }
     /// <summary>
     /// Point on map with coordinates and height above ground
     /// </summary>
-    internal class GIS_Point : GIS_Feature
+    public class GIS_Point : GIS_Feature
     {
         public Coordinates coordinates;
         public float height;
@@ -27,35 +27,35 @@ public class GIS_Feature
     /// <summary>
     /// Feature with many Points
     /// </summary>
-    internal class GIS_MultiPoint : GIS_Feature
+    public class GIS_MultiPoint : GIS_Feature
     {
         public GIS_Point[] points;
     }
     /// <summary>
     /// Line on map made with Points
     /// </summary>
-    internal class GIS_LineString : GIS_Feature
+    public class GIS_LineString : GIS_Feature
     {
         public GIS_Point[] points;
     }
     /// <summary>
     /// Polygon made of Line Strings
     /// </summary>
-    internal class GIS_Polygon : GIS_Feature
+    public class GIS_Polygon : GIS_Feature
     {
         public GIS_LineString[] linestrings;
     }
     /// <summary>
     /// Feature with many Polygons
     /// </summary>
-    internal class GIS_MultiPolygon : GIS_Feature
+    public class GIS_MultiPolygon : GIS_Feature
     {
         public GIS_Polygon[] polygons;
     }
     /// <summary>
     /// A collection containing several features
     /// </summary>
-    internal class GIS_GeometryCollection : GIS_Feature
+    public class GIS_GeometryCollection : GIS_Feature
     {
         public GIS_Feature[] geometries;
     }

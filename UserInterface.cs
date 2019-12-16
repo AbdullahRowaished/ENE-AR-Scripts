198b40ea8bf50b2d7a6be276ddfd079a758ecd27
Abdullah Rowaished <abdullah.rowaished.1@gmail.com>
2019-12-16 15:32:18 +0300

    feat: Launch Screen to AR Session UI Switch Code to switch from Launch Screen to AR Session

--- UserInterface.cs
+++ UserInterface.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UserInterface : MonoBehaviour
 {
+    // GameObjects for UI Elements
+    public GameObject Launch, Session; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Launch = GameObject.Find("LaunchScreen");
+        Session = GameObject.Find("ARSession");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GetStarted()
+    {
+        Launch.SetActive(false);
+        Session.SetActive(true);
     }
 }

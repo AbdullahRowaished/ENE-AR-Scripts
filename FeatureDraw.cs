ea09559c02a0d3b321fb1e3903b035b4cc90a11d
Abdullah Rowaished <abdullah.rowaished.1@gmail.com>
2019-12-16 19:10:43 +0300

    feat: Add function to draw GIS features in Scene

--- FeatureDraw.cs
+++ FeatureDraw.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class FeatureDraw : MonoBehaviour
 {
+    [Tooltip("Database of Geometries")]
+    public FeatureData features;
+
+    [Tooltip("Fiber Cable Prefab")]
+    public GameObject FiberPrefab;
+
+    [Tooltip("Geolocation Script")]
+    public Geolocation geolocation;
     // Start is called before the first frame update
-    void Start()
+    
+
+    public void DrawFeatures()
     {
-        
+        foreach (GIS_Feature feature in features.database.geometries)
+        {
+            if (feature is GIS_Feature.GIS_LineString)
+            {
+
+                GIS_Feature.GIS_LineString fiber_cable = ((GIS_Feature.GIS_LineString) feature);
+
+                for (int i = 0; i < fiber_cable.points.Length - 1; i++)
+                {
+                    GIS_Feature.GIS_Point firstPoint, secondPoint;
+
+                    firstPoint = ((GIS_Feature.GIS_Point)fiber_cable.points[i]);
+                    secondPoint = ((GIS_Feature.GIS_Point)fiber_cable.points[i+1]);
+
+                    //TODO
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
